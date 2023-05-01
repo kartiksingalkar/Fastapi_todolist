c0b6f262d27f2523b60284cf7acfa91081c25633
kartiksingalkar <kartiksingalkar6@gamail.com>
2023-05-02 01:53:01 +0530

    inital

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI , Request
+
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+
+
